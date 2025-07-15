28b0bb636bfa1aaea6ed4c6345e5be534bd997a2
Zach Williams <hey@zachwill.com>
2025-07-15 16:04:32 -0700

    More cleanup

--- src/pages/counter.tsx
+++ src/pages/counter.tsx
@@ -1,18 +1,21 @@
 import { html } from '../lib/sse';
+import { Datastar } from "../lib/datastar";
 import Shell from "../components/shell";
 
 export type Signals = {
-  counter: number;
+  counter?: number;
 };
+
+const $ = Datastar<Signals>();
 
 export const routes = {
   "/counter": () => html(
     <Shell>
       <h1>Counter</h1>
-      <div role="group" {...{ "data-signals": JSON.stringify({ counter: 0 }) }} style={{ alignItems: "center" }}>
-        <button {...{ "data-on-click": "$counter -= 1" }}>-</button>
-        <h2 style={{ textAlign: "center" }} {...{ "data-text": "$counter" }} />
-        <button {...{ "data-on-click": "$counter += 1" }}>+</button>
+      <div role="group" {...$({ counter: 0 })} style={{ alignItems: "center" }}>
+        <button {...{ "data-on-click": $`$counter -= 1` }}>-</button>
+        <h2 {...{ "data-text": $`$counter` }} style={{ textAlign: "center" }} />
+        <button {...{ "data-on-click": $`$counter += 1` }}>+</button>
       </div>
     </Shell>
   ),
--- src/pages/slider.tsx
+++ src/pages/slider.tsx
@@ -1,22 +1,25 @@
-import { sse, patchSignals, html } from "../lib/sse";
+import { html, sse, patchSignals } from "../lib/sse";
+import { Datastar } from "../lib/datastar";
 import Shell from "../components/shell";
 
 export type Signals = {
-  slider: number
+  slider?: number;
 };
+
+const $ = Datastar<Signals>();
 
 export const routes = {
   "/slider": () => html(
     <Shell>
       <h1>Slider</h1>
-      <div {...{ "data-signals": JSON.stringify({ slider: 0 }) }}>
+      <div {...$({ slider: 0 })}>
         <div className="grid">
           <input type="range" min="0" max="99"
             {...{
               "data-bind-slider": "",
-              "data-on-interval__duration.1s.leading": "@get('/sse/slider')",
+              "data-on-interval__duration.120ms.leading": "@get('/sse/slider')",
             }} />
-          <output {...{ "data-text": "$slider" }}></output>
+          <output {...{ "data-text": $`$slider` }} />
         </div>
       </div>
     </Shell>
@@ -25,7 +28,7 @@ export const routes = {
   "/sse/slider": sse(async function* (req: Request, signals: Record<string, any>) {
     const value = Number(signals.slider) || 0;
     yield patchSignals({
-      slider: (value + 4) % 100,
+      slider: (value + 2) % 100,
     });
   }),
 } as const;
