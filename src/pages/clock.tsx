d2289ad61aec9ccffb8a48ce26cd455074635761
Zach Williams <hey@zachwill.com>
2025-07-15 05:48:58 -0700

    Use 'sse' as endpoint

--- src/pages/clock.tsx
+++ src/pages/clock.tsx
@@ -8,14 +8,14 @@ export const routes = {
       <h1>Clock</h1>
       <h2 id="server-time"
         {...{
-          "data-on-interval__duration.1s.leading": "@get('/api/clock')",
+          "data-on-interval__duration.1s.leading": "@get('/sse/clock')",
           "data-on-signal-patch": "el.textContent = patch.clock"
         }}>
         --:--:--
       </h2>
     </Shell>
   ),
-  "/api/clock": channel(async function* (req: Request, signals: Record<string, any>) {
+  "/sse/clock": channel(async function* (req: Request, signals: Record<string, any>) {
     yield patchSignals({ clock: new Date().toLocaleTimeString() });
   }),
 } as const;
